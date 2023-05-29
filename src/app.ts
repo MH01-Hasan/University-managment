c2bef02d3c81cf085141c8a85d6f2377f21b044e
MH01-Hasan <mehediph2021@gmail.com>
2023-05-29 21:48:21 +0600

    setup ok

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', async (req: Request, res: Response) => {
-  res.send('hello World')
+  res.send('hello World University managnent')
 })
 
 export default app
