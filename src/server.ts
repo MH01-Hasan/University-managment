2a94214f194441ff7f310c67aa672c509eaf68c0
MH01-Hasan <mehediph2021@gmail.com>
2023-06-04 01:24:13 +0600

    setup logger

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
+import logger from './shared/Logger'
 
 async function main() {
   try {
     await mongoose.connect(config.Database_url as string)
 
     app.listen(config.port, () => {
-      console.log(`database conection Done and Listen port ${config.port}`)
+      logger.info(`database conection Done and Listen port ${config.port}`)
     })
   } catch (err) {
-    console.log('databse conection problem', err)
+    logger.error('databse conection problem', err)
   }
 }
 main()
